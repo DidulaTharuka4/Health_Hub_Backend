e7f17164ffd3d60f105b0d6a4b8f5cb202d6d235
Didula Tharuka <didulat00@gmail.com>
2025-05-25 13:12:13 +0530

    updated app run

--- app.py
+++ app.py
@@ -61,6 +61,9 @@ def predict():
         'confidence': float(np.max(prediction))
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
